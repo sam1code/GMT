9e0715fef0a096344ad4953b8afd94896bed55bb
sam-streak <soumyakanta.pattanaik@streak.tech>
2024-02-25 22:01:16 +0100

    done

---
+++ src/components/landing/header/page.js
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items
+  const navItems = [
+    { id: 1, text: "Home" },
+    { id: 2, text: "Services" },
+    { id: 3, text: "Blog" },
+    { id: 4, text: "Contact" },
+    { id: 5, text: "About" },
+  ];
+
+  return (
+    <div className=" flex justify-between items-center h-16 px-8  mx-auto md:px-8">
+      {/* Logo */}
+      <Image
+        src="https://flowbite.com/docs/images/logo.svg"
+        width={32}
+        height={32}
+        alt="logo"
+        className="mr-4 cursor-pointer"
+      />
+      <h1 className="w-full text-2xl font-bold">GMT Policies </h1>
+
+      {/* Desktop Navigation */}
+      <ul className="hidden md:flex">
+        {navItems.map((item) => (
+          <li
+            key={item.id}
+            className="p-4 hover:text-blue-700 rounded-xl m-2 cursor-pointer duration-300"
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile Navigation Icon */}
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+
+      {/* Mobile Navigation Menu */}
+      <ul
+        className={
+          nav
+            ? "fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#939693] ease-in-out duration-500"
+            : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
+        }
+      >
+        {/* Mobile Logo */}
+        <h1 className="w-full text-2xl font-bold m-4">GMT Policies</h1>
+
+        {/* Mobile Navigation Items */}
+        {navItems.map((item) => (
+          <li
+            key={item.id}
+            className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600"
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
